d8c1f180cb40762edfef887168d52f1ec0df4397
ndeta100 <ndetainnocent3@gmail.com>
2023-12-03 15:21:49 +0200

    room store for saving rooms

---
+++ db/room_store.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"context"
+	"github.com/Ndeta100/CamHotelConnect/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+)
+
+type RoomStore interface {
+	InsertRoom(ctx context.Context, room *types.Room) (*types.Room, error)
+}
+type MongoRoomStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+	//hotel store dependency
+	HotelStore
+}
+
+func NewMongoRoomStore(client *mongo.Client, hotelStore HotelStore) *MongoRoomStore {
+	return &MongoRoomStore{
+		client:     client,
+		coll:       client.Database(DBNAME).Collection("rooms"),
+		HotelStore: hotelStore,
+	}
+}
+
+func (s *MongoRoomStore) InsertRoom(ctx context.Context, room *types.Room) (*types.Room, error) {
+	resp, err := s.coll.InsertOne(ctx, room)
+	if err != nil {
+		return nil, err
+	}
+	room.ID = resp.InsertedID.(primitive.ObjectID)
+	//update hotel with room id
+	filter := bson.M{"_id": room.HotelID}
+	update := bson.M{"$push": bson.M{"rooms": room.ID}}
+	if err := s.HotelStore.UpdateHotel(ctx, filter, update); err != nil {
+		return nil, err
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return room, nil
+}
