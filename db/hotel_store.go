1b08cd58c46f39ec53c5566bf34d12d40d1391ed
ndeta100 <ndetainnocent3@gmail.com>
2023-12-03 15:22:12 +0200

    hotel store for saving hotels

---
+++ db/hotel_store.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"github.com/Ndeta100/CamHotelConnect/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+)
+
+type HotelStore interface {
+	InsertHotel(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error)
+	UpdateHotel(ctx context.Context, filter bson.M, update bson.M) error
+}
+type MongoHotelStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoHotelStore(client *mongo.Client) *MongoHotelStore {
+	return &MongoHotelStore{
+		client: client,
+		coll:   client.Database(DBNAME).Collection("hotels"),
+	}
+}
+
+func (s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error) {
+	resp, err := s.coll.InsertOne(ctx, hotel)
+	if err != nil {
+		return nil, err
+	}
+	hotel.ID = resp.InsertedID.(primitive.ObjectID)
+	return hotel, nil
+}
+
+func (s *MongoHotelStore) UpdateHotel(ctx context.Context, filter bson.M, update bson.M) error {
+	_, err := s.coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
