2cc471d804ae5aca8fdccc0cab1007c9695679a1
ndeta100 <ndetainnocent3@gmail.com>
2023-12-21 02:09:27 +0200

    custom snippet for admin auth

---
+++ api/middleware/admin.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/Ndeta100/CamHotelConnect/types"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AdminAuth(c *fiber.Ctx) error {
+	user, ok := c.Context().UserValue("user").(*types.User)
+	if !ok {
+		return fmt.Errorf("not authorized")
+	}
+	if !user.IsAdmin {
+		return fmt.Errorf("not authorized")
+	}
+	return c.Next()
+}
