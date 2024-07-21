3d2eef0ca59889276c81b335b8d6f9d48683de8a
ndeta100 <ndetainnocent3@gmail.com>
2024-07-21 13:41:44 +0300

    trying image uploads

--- types/hotel.go
+++ types/hotel.go
@@ -3,11 +3,14 @@ package types
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Hotel struct {
-	ID       primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
-	Name     string               `bson:"name" json:"name"`
-	Location string               `bson:"location" json:"location"`
-	Rooms    []primitive.ObjectID `bson:"rooms" json:"rooms"`
-	Rating   int                  `bson:"rating" json:"rating"`
+	ID          primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	Name        string               `bson:"name" json:"name"`
+	Location    string               `bson:"location" json:"location"`
+	Description string               `bson:"description" json:"description"`
+	Amenities   []string             `bson:"amenities" json:"amenities"`
+	Rooms       []primitive.ObjectID `bson:"rooms" json:"rooms"`
+	Rating      int                  `bson:"rating" json:"rating"`
+	Images      []Image              `bson:"images" json:"images"`
 }
 
 type Room struct {
@@ -17,4 +20,9 @@ type Room struct {
 	Size    string             `bson:"size" json:"size"`
 	Price   float64            `bson:"price" json:"price"`
 	HotelID primitive.ObjectID ` bson:"hotelID" json:"hotelID"`
+}
+
+type Image struct {
+	ID  primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	URL string             `bson:"url" json:"url"`
 }
